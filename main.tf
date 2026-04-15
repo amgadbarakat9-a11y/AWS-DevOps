a94b4615db71ad31dbf2d82b2aa84db8293441fc
Amgad Barakat <amgadbarakat98@gmail.com>
2026-04-15 02:45:10 +0200

    edite in main

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ module "network" {
     vpc_cidr=var.vpc_cidr
     subnet1_cidr=var.subnet1_cidr
     subnet2_cidr=var.subnet2_cidr
+    public_key_path = var.public_key_path
+  region          = var.region
 }
